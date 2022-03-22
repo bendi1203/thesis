e8ea009ccf0516fe777b8973a2d6cca18d242419
boss1203 <h983246@stud.u-szeged.hu>
2022-03-22 12:12:52 +0100

    overload update

--- FirstFitWithOverload.py
+++ FirstFitWithOverload.py
@@ -2,10 +2,10 @@ import sys
 
 def overload(c):
     if((0 <= c) and (c <= 3/2)):
-        c = sys.maxsize
+        c = 1 + sys.maxsize
     elif((3/2 <= c) and (c <= 9/5)):
-        c = 1/c
+        c = 1 + 1/c
     elif((9/5 <= c) and (c <= 14/3)):
-        c = 2/(3 * c)
+        c = 1 + 2/(3 * c)
     else:
-        c = 1/(3 * c)
+        c = 1 + 1/(3 * c)
