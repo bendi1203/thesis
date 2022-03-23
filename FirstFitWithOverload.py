3fc087b6964395767d816b9a8a3262659217e0a6
boss1203 <h983246@stud.u-szeged.hu>
2022-03-23 18:11:06 +0100

    firstfit function update

--- FirstFitWithOverload.py
+++ FirstFitWithOverload.py
@@ -2,10 +2,38 @@ import sys
 
 def overload(c):
     if((0 <= c) and (c <= 3/2)):
-        c = 1 + sys.maxsize
+        s = 1 + sys.maxsize
+        return s
+
     elif((3/2 <= c) and (c <= 9/5)):
-        c = 1 + 1/c
+        s = 1 + 1/c
+        return s
+
     elif((9/5 <= c) and (c <= 14/3)):
-        c = 1 + 2/(3 * c)
+        s = 1 + 2/(3 * c)
+        return s
+
     else:
-        c = 1 + 1/(3 * c)
+        s = 1 + 1/(3 * c)
+        return s
+
+def firstfit(items, n, c):   #   p: elemek súlya, n : elemek darabszáma, c túltöltési költség
+        h = 0   # felhasznált ládák száma
+        s = overload(c)  # láda magassága túltöltéssel
+        d = [0] * n
+
+        for i in range(n): #  végigmegy az elemeken
+            j = 0
+            while j < h: #  végigmegy a ládákon
+                if d[j] >= items[i]:
+                    print('if-ben d[j]: ', d[j], 'if-ben p[j]: ', items[j])
+                    d[j] -= items[i]
+                    break
+                j += 1
+
+            if (j == h): #  végigmegy és nem rakja bele egyikbe sem
+                d[h] = s - items[j]
+                h += 1
+        return h
+
+print("Használt ládák száma: ",firstfit([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 10, 2))
