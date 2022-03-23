0ae5340a78e8ae6eef5ffb8c23f90b24c75e36bb
boss1203 <h983246@stud.u-szeged.hu>
2022-03-23 19:07:37 +0100

    input

--- FirstFitWithOverload.py
+++ FirstFitWithOverload.py
@@ -1,39 +1,58 @@
 import sys
+import os
+
+path = r"D:\Thesis\thesis\input_bison" #   nem működik input_bison-nal
+os.chdir(path)  # belép a könyvtárba
+
+
+#   törlendő
+def read_text(fpath):
+    with open(fpath, 'r') as f:
+        print(f.read())
+#   törlendő
+
+for file in os.listdir():
+    if file.endswith('.BPP'):
+        fpath = f"{path}\{file}"
+        read_text(fpath)  # törlendő
+        print('---')  # törlendő
+
 
 def overload(c):
-    if((0 <= c) and (c <= 3/2)):
+    if ((0 <= c) and (c <= 3 / 2)):
         s = 1 + sys.maxsize
         return s
 
-    elif((3/2 <= c) and (c <= 9/5)):
-        s = 1 + 1/c
+    elif ((3 / 2 <= c) and (c <= 9 / 5)):
+        s = 1 + 1 / c
         return s
 
-    elif((9/5 <= c) and (c <= 14/3)):
-        s = 1 + 2/(3 * c)
+    elif ((9 / 5 <= c) and (c <= 14 / 3)):
+        s = 1 + 2 / (3 * c)
         return s
 
     else:
-        s = 1 + 1/(3 * c)
+        s = 1 + 1 / (3 * c)
         return s
 
-def firstfit(items, n, c):   #   p: elemek súlya, n : elemek darabszáma, c túltöltési költség
-        h = 0   # felhasznált ládák száma
-        s = overload(c)  # láda magassága túltöltéssel
-        d = [0] * n
 
-        for i in range(n): #  végigmegy az elemeken
-            j = 0
-            while j < h: #  végigmegy a ládákon
-                if d[j] >= items[i]:
-                    print('if-ben d[j]: ', d[j], 'if-ben p[j]: ', items[j])
-                    d[j] -= items[i]
-                    break
-                j += 1
+def firstfit(items, n, c):  # p: elemek súlya, n : elemek darabszáma, c túltöltési költség
+    h = 0  # felhasznált ládák száma
+    s = overload(c)  # láda magassága túltöltéssel
+    d = [0] * n
 
-            if (j == h): #  végigmegy és nem rakja bele egyikbe sem
-                d[h] = s - items[j]
-                h += 1
-        return h
+    for i in range(n):  # végigmegy az elemeken
+        j = 0
+        while j < h:  # végigmegy a ládákon
+            if d[j] >= items[i]:
+                d[j] -= items[i]
+                break
+            j += 1
 
-print("Használt ládák száma: ",firstfit([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 10, 2))
+        if (j == h):  # végigmegy és nem rakja bele egyikbe sem
+            d[h] = s - items[j]
+            h += 1
+    return h
+
+
+print("Használt ládák száma: ", firstfit([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 10, 2))
