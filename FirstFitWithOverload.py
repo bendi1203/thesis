8198f42cfb9cccef87dc90d6c7d70c7bfba93f8b
boss1203 <h983246@stud.u-szeged.hu>
2022-03-26 20:27:50 +0100

    update

--- FirstFitWithOverload.py
+++ FirstFitWithOverload.py
@@ -1,16 +1,64 @@
 import sys
 import os
 
-# c-t ide megadni
-# firstfitet egyesével az elemekre meghívni
 path = r"D:\Thesis\thesis\input_bison"  # nem működik input_bison-nal
 os.chdir(path)
+
+cost = 2
+
+
+def overload(c):
+    if (0 <= c) and (c <= 3 / 2):
+        s = 1 + sys.maxsize
+
+    elif (3 / 2 <= c) and (c <= 9 / 5):
+        s = 1 + 1 / c
+        print('overload')
+
+    elif (9 / 5 <= c) and (c <= 14 / 3):
+        s = 1 + 2 / (3 * c)
+
+    else:
+        s = 1 + 1 / (3 * c)
+    return s * 100
+
+
+def firstFitForOneItem(bins, remainingSpace, item, maxHeight):
+    j = 0
+    while j < bins:
+        if remainingSpace[j] >= item:
+            remainingSpace[j] -= item
+            break
+        j += 1
+        if j == bins:
+            remainingSpace.append(maxHeight - item)
+            bins += 1
+            break
+    # print(bins)
+    return bins
+
+
+def firstFit(overloadCost, data):
+    usedBins = 1
+    maxHeight = overload(overloadCost)
+    remainingSpace = []
+    remainingSpace.append(maxHeight)
+
+    for i in range(len(data)):
+        usedBins = firstFitForOneItem(usedBins, remainingSpace, data[i], maxHeight)
+
+    return usedBins - 1
 
 
 def read_text(fpath):
     with open(fpath, 'r') as f:
-        data = [file.split() for file in f]
-        dataInt = [list( map(int,i) ) for i in data]
+        f.readline()
+        data = f.readlines()
+        dataInt = []
+        for i in range(len(data)):
+            dataInt.append(int(data[i]))
+        result = firstFit(cost, dataInt)
+        print(result)
 
 
 for file in os.listdir():
@@ -18,47 +66,7 @@ for file in os.listdir():
         fpath = f"{path}\{file}"
         read_text(fpath)
 
-
-
-def overload(c):
-    if (0 <= c) and (c <= 3 / 2):
-        s = 1 + sys.maxsize
-        return s
-
-    elif (3 / 2 <= c) and (c <= 9 / 5):
-        s = 1 + 1 / c
-        return s
-
-    elif (9 / 5 <= c) and (c <= 14 / 3):
-        s = 1 + 2 / (3 * c)
-        return s
-
-    else:
-        s = 1 + 1 / (3 * c)
-        return s
-
-
-def firstfit(items, numberOfItems, overloadCost):
-    usedBins = 0
-    maxHeight = overload(overloadCost)
-
-    remainingSpace = [0] * numberOfItems
-
-    for i in range(numberOfItems):
-        j = 0
-        while j < usedBins:
-            if remainingSpace[j] >= items[i]:
-                remainingSpace[j] -= items[i]
-                break
-            j += 1
-
-        if j == usedBins:
-            remainingSpace[usedBins] = maxHeight - items[j]
-            usedBins += 1
-    return usedBins
-
-
-print("Használt ládák száma: ", firstfit([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 10, 2))
+# print("Használt ládák száma: ", firstFit([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 10, 2))
 
 # h: rekeszek száma
 # wi: i-edik rekesznek a magassága (a benne lévő dobozok összemagassága)
